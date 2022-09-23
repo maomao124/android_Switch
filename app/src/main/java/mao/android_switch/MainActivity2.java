8c1c9b44ea3605cefced9e49c721304dd3e813cd
mao <1296193245@qq.com>
2022-09-23 21:24:37 +0800

    android_Switch

--- app/src/main/java/mao/android_switch/MainActivity2.java
+++ app/src/main/java/mao/android_switch/MainActivity2.java
@@ -1,16 +1,75 @@
 package mao.android_switch;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity
 {
 
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Switch switch1 = findViewById(R.id.switch1);
+        Switch switch2 = findViewById(R.id.switch2);
+        Switch switch3 = findViewById(R.id.switch3);
+
+        switch1.setChecked(true);
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if (isChecked)
+                {
+                    Toast.makeText(MainActivity2.this, "WiFi已打开", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity2.this, "WiFi已关闭", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if (isChecked)
+                {
+                    Toast.makeText(MainActivity2.this, "蓝牙已打开", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity2.this, "蓝牙已关闭", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        switch3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if (isChecked)
+                {
+                    Toast.makeText(MainActivity2.this, "移动数据已打开", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity2.this, "移动数据已关闭", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
